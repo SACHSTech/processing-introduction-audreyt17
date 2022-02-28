fa5918794b8bdc8ae1b2cfa7afa4b10339256139
Audrey Tao <100211854+audreyt17@users.noreply.github.com>
2022-02-28 15:18:04 +0000

    5.2 Processing Task 1 - Calling Functions

--- Sketch.java
+++ Sketch.java
@@ -16,20 +16,59 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(161, 185, 207);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    float flowerX = 300;
+    float flowerY = 270;
+    float petalSize = 50;
+    float petalDistance = petalSize / 2;
+
+    fill(195, 149, 199);
+
+// upper-left petal
+    ellipse(flowerX - petalDistance, flowerY - petalDistance,
+        petalSize, petalSize);
+
+// upper-right petal
+    ellipse(flowerX + petalDistance, flowerY - petalDistance,
+        petalSize, petalSize);
+
+// lower-left petal
+    ellipse(flowerX - petalDistance, flowerY + petalDistance,
+        petalSize, petalSize);
+
+// lower-right petal
+    ellipse(flowerX + petalDistance, flowerY + petalDistance,
+        petalSize, petalSize);
+
+// center petal
+    fill(248, 252, 131);
+    ellipse(flowerX, flowerY,
+        petalSize, petalSize);
+
+// flower stem
+    stroke(128);
+    strokeWeight(3/2);
+    line(300, 400, 300, 305);
+
+// rectangle of the house
+    fill(135, 108, 68);
+    rect(50, 149, 160, 250);
+    
+// door of the house
+    fill(189, 154, 102);
+    rect(90, 270, 80, 270);
+
+// grass
+    fill(97, 173, 97);
+    rect(0, 370, 400, 30);
+    
   }
   
   // define other methods down here.
